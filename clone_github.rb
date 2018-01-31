5c9d277934150196484bdc289b551b9d62b59b3e
Tammy Cravit <tammycravit@me.com>
2018-01-31 15:36:09 -0500

    Updated clone_github.rb to use TcravitRubyLib app banner code

--- clone_github.rb
+++ clone_github.rb
@@ -12,11 +12,11 @@ require 'fileutils'
 require 'tcravit_ruby_lib'
 require 'git'
 
-puts "****************************************************************************"
-puts "* Clone all of your github repos into a project directory                  *"
-puts "*                                                                          *"
-puts "* Version 1.00, Wed Jan 31 12:27:53 EST 2018, tammy.cravit                 *"
-puts "****************************************************************************"
+puts TcravitRubyLib::Banner(name: "clone_github.rb",
+											      description: "Clone all github repos",
+											      version: "1.1", 
+											      date: "2018-01-31", 
+											      author: "Tammy Cravit")
 puts ""
 
 GITHUB_USER_NAME = "tammycravit"
@@ -42,14 +42,22 @@ response = http.request(request)
 
 repo_data = JSON.parse(response.body)
 
+num_cloned = 0
+num_checked = 0
+
 puts "* Checking/cloning Repos:"
 repo_data.each do |r|
 	repo_name = r["name"]
+	num_checked = num_checked + 1
 
 	if (Dir.exist?(File.join(project_dir, repo_name))) and (Dir.exist?(File.join(project_dir, repo_name, ".git"))) then
 		puts "    - Found repo clone for #{repo_name} - skipping"
 	else
 		puts "    - Cloning #{repo_name}"
 		Git.clone "git@github.com:#{GITHUB_USER_NAME}/#{repo_name}.git", File.join(project_dir, repo_name)
+		num_cloned = num_cloned + 1
 	end
 end
+
+puts "* Done. Cloned #{num_cloned} of #{num_checked} repos."
+exit 0
